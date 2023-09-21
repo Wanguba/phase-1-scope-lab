4fcad554ed741ee2dbd5b005b42067962bace8fe
Edith Wanguba <wanguamor@gmail.com>
2023-09-21 16:00:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+  var bestCustomer;
+
+  function setBestCustomer() {
+    bestCustomer = undefined;
+  }
+
+  function setBestCustomer(){
+    bestCustomer = 'not bob';
+  }
+
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+}
+
+const leastFavoriteCustomer = 'initialValue'; // Define and initialize leastFavoriteCustomer
+
+function changeLeastFavoriteCustomer(newLeastFavoriteCustomer) {
+  leastFavoriteCustomer = newLeastFavoriteCustomer;
+}
+
